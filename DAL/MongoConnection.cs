7b877502d0200885daed482ecf56864679918532
Pankil Shah <pnklshh06@hotmail.com>
2021-08-09 15:17:36 -0500

    Added DnsClient package and removed bin and obj folders grom git

--- DAL/MongoConnection.cs
+++ DAL/MongoConnection.cs
@@ -5,6 +5,7 @@ using System.Web;
 using MongoDB.Driver;
 using MongoDB.Driver.Core;
 using MongoDB.Bson;
+using System.Configuration;
 
 namespace InventoryManagementWebAPI.DAL
 {
@@ -13,7 +14,9 @@ namespace InventoryManagementWebAPI.DAL
         public IMongoDatabase database;
         public MongoConnection()
         {
-            string connectionString = "mongodb://localhost:27017";
+            //string connectionString = "mongodb://localhost:27017";
+            //string connectionString = "mongodb+srv://admin:admin123@testcluster.vu5lj.mongodb.net/test";
+            string connectionString = ConfigurationManager.AppSettings["MongoConnectionString"];
             MongoClient client = new MongoClient(connectionString);
             database = client.GetDatabase("InventoryDB");
         }
--- obj/Release/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InventoryManagementWebAPI.WebApiApplication" Language="C#" %>
